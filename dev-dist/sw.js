899815d79aad667995d06922c64577edbb3455ab
Enrique Hernandez Montiel <enriquehmsb@gmail.comgit congig --global -egit config --global user.email enriquehmsb@gmail.com>
2026-06-24 01:08:13 -0600

    configs

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "bafe3c85907c1bcaa9eb13e25f54524c"
   }, {
     "url": "index.html",
-    "revision": "0.svbfbnn8e78"
+    "revision": "0.7duv5sp0g8g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
